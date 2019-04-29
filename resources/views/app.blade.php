f05bf8288941cc02a75f962b571c1e9db0fc1c4e
Masayuki Nii <masayukinii1011@gmail.com>
2019-04-30 00:28:16 +0900

    create basic todo function

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,33 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <title>Laravel</title>
-    </head>
-    <body>
-        <div id="app">
-        <h2>Contetns</h2>
-        <router-view></router-view>
-        </div>
-        <script src="{{ mix('js/app.js') }}"></script>
-    </body>
+  <head>
+    <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+      <title>Laravel</title>
+  </head>
+  <body>
+    <div id="app">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="タスクを入力してください" v-model="new_todo">
+        <span class="input-group-btn"><button class="btn btn-success" type="button" v-on:click="addTodo">タスクを登録する</button></span>
+      </div>
+      <table class="table">
+        <tr>
+          <th>ID</th>
+          <th>タスク名</th>
+          <th>完了ボタン</th>
+				</tr>
+    		<tr v-for="todo in todos" v-bind:key="todo.id">
+      		<td>@{{ todo.id }}</td>
+      		<td>@{{ todo.title }}</td>
+      		<td><button class="btn btn-primary" v-on:click="deleteTodo(todo.id)">完了</button></td>
+    		</tr>
+			</table>
+      <!-- <router-view></router-view> -->
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+  </body>
 </html>
