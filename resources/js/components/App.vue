844a49b880ffb7219e7add44dee78da1901e9fba
Masayuki Nii <masayukinii1011@gmail.com>
2019-05-04 22:38:23 +0900

    todosのスタイル修正

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,7 +2,9 @@
 div#app
   div.wrap
     header-component
-    router-view
+    div.main
+      div.main-inner
+        router-view
   top-button
   footer-component
 </template>
@@ -17,3 +19,14 @@ export default {
     }
   }
 </script>
+
+<style>
+.main-inner {
+    width:896px;
+    margin: 0 auto;
+    padding:32px;
+    background:#fff;
+    border-radius: 2px;
+    box-shadow: 0 0 2px 0 rgba(0,0,0,0.14), 0 0 1px -2px rgba(0,0,0,0.12), 0 0 5px 0 rgba(0,0,0,0.2);
+}
+</style>
--- resources/js/components/Todos.vue
+++ resources/js/components/Todos.vue
@@ -1,19 +1,18 @@
 <template lang="pug">
 div
-  div.input-group
-    input.form-control(type="text" placeholder="タスクを入力してください" v-model="new_todo")
-    span.input-group-btn
-      button.btn.btn-success(type="button" v-on:click="addTodo") タスクを登録する
-    table.table
-      tr
-        th ID
-        th タスク名
-        th 完了ボタン
-      tr(v-for="todo in todos" v-bind:key="todo.id")
-        td {{ todo.id }}
-        td {{ todo.title }}
-        td
-          button.btn.btn-primary(v-on:click="deleteTodo(todo.id)") 完了
+  div.input-new-wrap
+    input(type="text" placeholder="Enter your todo." v-model="new_todo")
+    p.float-btn.add-btn(type="button" v-on:click="addTodo") Add Todo
+  table
+    tr
+      th No.
+      th Todo
+      th
+    tr(v-for="todo in todos" v-bind:key="todo.id")
+      td {{ todo.id }}
+      td {{ todo.title }}
+      td
+        p.float-btn.ok-btn(v-on:click="deleteTodo(todo.id)") OK
 </template>
 
 <script>
@@ -53,3 +52,72 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.input-new-wrap {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom:40px;
+
+  input {
+    padding:6px;
+    font-size:20px;
+    width:80%;
+    border-radius:4px;
+    border:2px solid #ccc;
+    background:#fdfdfd;
+  }
+}
+
+th,td {
+  font-size:20px;
+}
+
+.float-btn {
+  cursor:pointer;
+  color: #fff;
+  text-transform:uppercase;
+  border-radius: 8px;
+  font-size: 20px;
+  display: inline-block;
+  padding: 12px 16px;
+  transition: .2s ease-out;
+  box-shadow: 0 2px 1px 0 rgba(0,0,0,0.14), 0 3px 0px -2px rgba(0,0,0,0.12), 0 1px 3px 0 rgba(0,0,0,0.2);
+
+  &:hover {
+    box-shadow: 0 3px 2px 0 rgba(0, 0, 0, 0.14), 0 4px 1px -2px rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.2);
+    transform: scale(1.04);
+  }
+
+  &:active {
+    transform: translateY(2px);
+  }
+
+  &.add-btn {
+    background:#2196f3;
+
+    &:hover {
+      background:#1976d2;
+    }
+
+    &:active {
+      color:#4caf50;
+    }
+  }
+
+  &.ok-btn {
+    background:#ff5722;
+    margin:8px 16px;
+    padding: 10px 14px;
+    font-size:20px;
+
+    &:hover {
+      background:#e64a19;
+    }
+    
+    &:active {
+      color:#ab47bc;
+    }
+  }
+}
+</style>
